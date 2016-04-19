7acbaf8adcfe2acae1f28fdd322f8ee5a358d5f9
Praveen Chavan <pchavan@codeaurora.org>
2016-04-19 04:26:34 -0700

    mm-video-legacy: venc: update metadata-type used for HAL1 based  recording
    Update metadata buffer type from kMetadataBufferTypeCameraSource to
    kMetadataBufferTypeNativeHandleSource as the latter is used by
    camera-HALv1 that resides in a separate process.
    
    Surface recording uses a different metadata as compared to
    legacy camera metadata (HAL1).Cast the gralloc-meta to
    appropriate struct to extract the handle
    
    Since buffer-size is not known conclusively at the allocation
    time of meta-handles, allow the least size to be allocated.
    But check the buffer size before accessing in ETB
    
    Bug: 26268807
    Change-Id: I2cb40889517b4d740763bbde09e2fa4646b919e1

--- msm8974/mm-video-legacy/vidc/venc.mk
+++ msm8974/mm-video-legacy/vidc/venc.mk
@@ -35,6 +35,9 @@ endif
 ifeq ($(TARGET_USES_ION),true)
 libmm-venc-def += -DUSE_ION
 endif
+ifeq ($(TARGET_USES_MEDIA_EXTENSIONS),true)
+libmm-venc-def += -DUSE_NATIVE_HANDLE_SOURCE
+endif
 
 venc-inc       := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
